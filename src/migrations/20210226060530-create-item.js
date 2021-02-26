d7cf7617dbab9911753194b3df1ca78b181a097c
Pushya Tiwari <pushya.tiwari@gmail.com>
2021-02-26 11:52:01 +0530

    feat: add database and create tables

---
+++ src/migrations/20210226060530-create-item.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      categoryName: {
+        type: Sequelize.STRING
+      },
+      itemId: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      features: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Items');
+  }
+};
